f2f3bda5ba53fc08aa7d11ac110756d4a07677ac
DELL <babaralyy11@gmail.com>
2024-10-29 22:25:09 +0500

    VersatileMediaPicker

--- VersatileMediaPicker/build.gradle.kts
+++ VersatileMediaPicker/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("maven-publish")
 }
+group = "com.github.jitpack"
+version = "1.0"
 
 android {
     namespace = "com.mid.versatilemediapicker"
